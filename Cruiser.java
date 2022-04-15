9fb09b3095f16c6cd5e547e23acd0c814654fff2
ThanhNguyen122001 <91386062+ThanhNguyen122001@users.noreply.github.com>
2022-04-15 10:02:10 -0400

    Delete Cruiser.java

--- Cruiser.java
+++
@@ -1,9 +0,0 @@
-/*****************************************************************
-Creates the Cruiser 
-*****************************************************************/
-public class Cruiser extends Ship{
-	public Cruiser(boolean vertical) {
-		super(2, 'c', vertical);
-		this.setName("Cruiser");
-	}
-}
